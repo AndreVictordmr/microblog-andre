54e6f37c022a77cd48e51a944e16e07bae671518
AndreVictorDmr <41845560+AndreVictordmr@users.noreply.github.com>
2025-10-29 15:04:43 -0300

    validaçao de formulario PHP

--- admin/usuario-insere.php
+++ admin/usuario-insere.php
@@ -1,4 +1,22 @@
 <?php 
+require_once "../src/Model/Usuario.php";
+
+//Vareiavel que sera usada para montar mensagens de erros personalizadas
+$erro = null;
+
+if($_SERVER['REQUEST_METHOD']==='POST'){
+	// Validação do prenchimento dos campos
+	if(empty($_POST['nome'])||empty($_POST['senha'])||empty($_POST['email'])||empty($_POST['tipo'])){
+		$erro = "Preencha todos os campos";
+	}else{
+		echo "campos ok";
+	}
+	
+}
+
+
+
+
 require_once "../includes/cabecalho-admin.php";
 
 ?>
@@ -10,7 +28,10 @@ require_once "../includes/cabecalho-admin.php";
 		<h2 class="text-center">
 		Inserir novo usuário
 		</h2>
-				
+		<!--O paragrafo abaixo irá aparecer SOMENTE se houver algum erro. E neste caso, exibira a menssagem de erro-->
+		<?php if($erro){ ?>
+			<p class="alert alert-danger text-center"><?=$erro?></p>
+		<?php } ?>
 		<form class="mx-auto w-75" action="" method="post" id="form-inserir" name="form-inserir" autocomplete="off">
 
 			<div class="mb-3">
